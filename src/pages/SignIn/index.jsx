3a51ca2e3c1d6a51821b35da4e2b255962ed2928
Isabela <isabela_cmoreira@hotmail.com>
2022-05-09 12:15:42 -0300

    Updated the SignIn page using async functions

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,14 @@
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import Link from "../../components/Link";
+import Form from "../../components/Form"
 import Container from "../../components/Container";
 import Logo from "./style";
 import { useNavigate } from "react-router-dom";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from 'react-loader-spinner';
 import { useState, useEffect } from "react";
+import api from "../../services/api"
 
 
 
@@ -18,7 +20,7 @@ export default function SignIn() {
   
     useEffect(() => {
       if (auth && auth.token) {
-        navigate("/today");
+        navigate("/homepage");
       }
     }, []);
   
@@ -26,34 +28,60 @@ export default function SignIn() {
       setFormData({ ...formData, [e.target.name]: e.target.value });
     }
   
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
       e.preventDefault();
   
       setIsLoading(true);
-      const promise = api.login({ ...formData });
-      promise.then((response) => {
-        setIsLoading(false);
-  
-        login(response.data);
-        navigate("/today");
-      });
-      promise.catch(() => {
-        setIsLoading(false);
-  
-        alert('Erro, tente novamente');
-      });
+    const user = { ...formData };
+
+    try {
+      const { data } = await api.login(user);
+      setIsLoading(false);
+      login(data);
+      navigation('/homepage');
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      alert("Erro, tente novamente");
     }
+  }
   
     return (
         <Container>
-        <Logo>MyWallet</Logo>
-        <Input> E-mail</Input>
-        <Input> Senha </Input>
-        <Button> Entrar </Button>
-        <Link> Primeira vez? Cadastre-se!</Link>
+            <Logo>MyWallet</Logo>
+                <Form onSubmit={handleSubmit}>
+                    <Input 
+                        type="email"
+                        placeholder="E-mail"
+                        name="email"
+                        onChange={handleChange}
+                        value={formData.email}
+                        disabled={isLoading}
+                        required
+                    />             
+                    <Input
+                        type="password"
+                        placeholder="Senha"
+                        name="password"
+                        onChange={handleChange}
+                        value={formData.password}
+                        disabled={isLoading}
+                        required
+                    /> 
+                    <Button type="submit" disabled={isLoading}>
+                        {
+                            isLoading
+                            ? <Loader type="ThreeDots" color="#FFFFFF" height={50} width={50} />
+                            : "Entrar"
+                        }
+                    </Button>
+                </Form>
+
+                <Link to="/register"> 
+                Primeira vez? Cadastre-se!
+                </Link>
         </Container>
     )
 }
 
-
-    
+  
