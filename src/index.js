1f9867b0c5acc2ec7d636ded01965dcfba752621
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-05-18 16:39:38 -0300

    Preparando revisão de código

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "../src/styles/style.css";
-import "../src/styles/reset.css";
-
-ReactDOM.render(<App />, document.querySelector('.root'));
